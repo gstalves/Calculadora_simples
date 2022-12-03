f3888c14e8d7f442641b4f749bdf93c0a2458945
Gustavo <gustavoscalves@gmail.com>
2022-12-03 16:45:15 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #808080;
+    color: #FFF;
+    font-size: 25px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+        cursor: pointer;
+    }
+`
---
+++ src/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #D3D3D3;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #696969;
+    width: 40%;
+`
+
+export const Row = styled.div`
+    display: Flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
